f4e0d2afa9f930bf00343ba44dc4d894a8534238
tmhao3110 <minhhao3110@gmail.com>
2021-01-28 13:42:22 +0700

    hao
    layout-header

--- extension/assets/contentscript.js
+++ extension/assets/contentscript.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(
-    function(request,sender,sendResponse) {
-        if(request.message === 'urlchanged') {
+    function (request, sender, sendResponse) {
+        if (request.message === 'urlchanged') {
             init()
         }
     }
@@ -8,18 +8,18 @@ chrome.runtime.onMessage.addListener(
 function getUrlParams(hashKey = null) {
     try {
         const searchString = window.location.search
-        const params = new URLSearchParams(searchString),dict = {}
-        for(const [key,value] of params.entries()) dict[key] = value
-        if(hashKey) return dict[hashKey]
+        const params = new URLSearchParams(searchString), dict = {}
+        for (const [key, value] of params.entries()) dict[key] = value
+        if (hashKey) return dict[hashKey]
         else return dict
-    } catch(e) {console.log(e)}
+    } catch (e) { console.log(e) }
 }
 async function init() {
     try {
         console.log('init')
         const id = await getUrlParams("v")
         let channelId = ""
-        if(id) {
+        if (id) {
             let url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=AIzaSyDPpPlIMj5o_W3Q5B7qmn5-ex0kimioSiQ`
             const options = await {
                 method: 'GET',
@@ -27,38 +27,38 @@ async function init() {
                     'Content-Type': 'application/json'
                 }
             };
-            const request = await fetch(url,options)
+            const request = await fetch(url, options)
             const data = await request.json()
             channelId = data?.items[0].snippet?.channelId
         }
-        if(channelId === "" && window.location.href.includes("https://www.youtube.com/channel/")) {
+        if (channelId === "" && window.location.href.includes("https://www.youtube.com/channel/")) {
             const regChannel = await /(?<=https:\/\/www\.youtube\.com\/channel\/)[\w].+/g
             channelId = await await window.location.href.match(regChannel)[0]
         }
         let isMatchAlias = false
-        if(channelId === "" && window.location.href.includes("https://www.youtube.com/c/")) {
+        if (channelId === "" && window.location.href.includes("https://www.youtube.com/c/")) {
             const regAlias = await /(?<=https:\/\/www\.youtube\.com\/c\/)[\w].+/g
             channelId = await window.location.href.match(regAlias)[0]
             isMatchAlias = await true
         }
-        await chrome.storage.sync.get(['channels'],function(result) {
-            if(result.channels) {
+        await chrome.storage.sync.get(['channels'], function (result) {
+            if (result.channels) {
                 let channels = JSON.parse(result.channels)
-                if(isMatchAlias) {
-                    for(const [key,value] of Object.entries(channels)) {
-                        if(value.customUrl === channelId) {
+                if (isMatchAlias) {
+                    for (const [key, value] of Object.entries(channels)) {
+                        if (value.customUrl === channelId) {
                             window.location = "https://www.youtube.com";
                             break;
                         }
                     }
                 } else {
-                    if(channels[channelId] !== undefined) {
+                    if (channels[channelId] !== undefined) {
                         window.location = "https://www.youtube.com"
                     }
                 }
-            } else {}
+            } else { }
         });
-    } catch(e) {console.log(e)}
+    } catch (e) { console.log(e) }
 }
 init()
 
