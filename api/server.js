2b43584bd66ea03ab0e221127f7fc0366bb342c6
bao <bao@example.com>
2021-01-28 13:13:54 +0700

    deploy api

--- api/server.js
+++ api/server.js
@@ -13,6 +13,6 @@ db.once('open', () => console.log('Connected to Database'))
 app.use(express.json())
 
 const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+app.use('/', subscribersRouter)
 
 app.listen(3000, () => console.log('Server Started'))
