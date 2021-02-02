48b393540d5c00542e6523acac80663b2f5ac732
bao <bao@example.com>
2021-02-02 10:20:31 +0700

    Bao

--- extension/assets/contentscript.js
+++ extension/assets/contentscript.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === 'urlchanged') {
+    function(request,sender,sendResponse) {
+        if(request.message === 'urlchanged') {
             init()
         }
     }
@@ -8,18 +8,24 @@ chrome.runtime.onMessage.addListener(
 function getUrlParams(hashKey = null) {
     try {
         const searchString = window.location.search
-        const params = new URLSearchParams(searchString), dict = {}
-        for (const [key, value] of params.entries()) dict[key] = value
-        if (hashKey) return dict[hashKey]
+        const params = new URLSearchParams(searchString),dict = {}
+        for(const [key,value] of params.entries()) dict[key] = value
+        if(hashKey) return dict[hashKey]
         else return dict
-    } catch (e) { console.log(e) }
+    } catch(e) {console.log(e)}
+}
+function includes(url) {
+    return window.location.href.includes(url)
+}
+function matchReg(reg) {
+    return window.location.href.match(reg)[0]
 }
 async function init() {
     try {
         console.log('init')
         const id = await getUrlParams("v")
-        let channelId = ""
-        if (id) {
+        let channelId = null
+        if(id) {
             let url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=AIzaSyDPpPlIMj5o_W3Q5B7qmn5-ex0kimioSiQ`
             const options = await {
                 method: 'GET',
@@ -27,38 +33,40 @@ async function init() {
                     'Content-Type': 'application/json'
                 }
             };
-            const request = await fetch(url, options)
+            const request = await fetch(url,options)
             const data = await request.json()
             channelId = data?.items[0].snippet?.channelId
         }
-        if (channelId === "" && window.location.href.includes("https://www.youtube.com/channel/")) {
-            const regChannel = await /(?<=https:\/\/www\.youtube\.com\/channel\/)[\w].+/g
-            channelId = await await window.location.href.match(regChannel)[0]
+        if(!channelId && includes("https://www.youtube.com/channel/")) {
+            channelId = await matchReg(/((?<=https:\/\/www\.youtube\.com\/channel\/)[\w].+)(?=\?)/g)
         }
         let isMatchAlias = false
-        if (channelId === "" && window.location.href.includes("https://www.youtube.com/c/")) {
-            const regAlias = await /(?<=https:\/\/www\.youtube\.com\/c\/)[\w].+/g
-            channelId = await window.location.href.match(regAlias)[0]
+        if(!channelId && includes("https://www.youtube.com/c/")) {
+            channelId = await matchReg(/((?<=https:\/\/www\.youtube\.com\/c\/)[\w].+)(?=\?)/g)
             isMatchAlias = await true
         }
-        await chrome.storage.sync.get(['channels'], function (result) {
-            if (result.channels) {
+        if(!channelId && includes("https://www.youtube.com/user/")) {
+            channelId = await matchReg(/((?<=https:\/\/www\.youtube\.com\/user\/)[\w].+)(?=\?)/g)
+            isMatchAlias = await true
+        }
+        await chrome.storage.sync.get(['channels'],function(result) {
+            if(result.channels) {
                 let channels = JSON.parse(result.channels)
-                if (isMatchAlias) {
-                    for (const [key, value] of Object.entries(channels)) {
-                        if (value.customUrl === channelId) {
+                if(isMatchAlias) {
+                    for(const [key,value] of Object.entries(channels)) {
+                        if(value.customUrl === channelId) {
                             window.location = "https://www.youtube.com";
                             break;
                         }
                     }
                 } else {
-                    if (channels[channelId] !== undefined) {
+                    if(channels[channelId] !== undefined) {
                         window.location = "https://www.youtube.com"
                     }
                 }
-            } else { }
+            } else {}
         });
-    } catch (e) { console.log(e) }
+    } catch(e) {console.log(e)}
 }
 init()
 
