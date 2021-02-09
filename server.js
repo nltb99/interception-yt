bca14507c8e98ee45bc5b8baa23c763d8bc663d5
baonlt99 <nguyenletienbao99@gmail.com>
2021-02-09 13:31:18 +0700

    Adjust

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 require('dotenv').config()
 
 const express = require('express'),
- app = express(),
- cors = require('cors'),
- mongoose = require('mongoose')
+    app = express(),
+    cors = require('cors'),
+    mongoose = require('mongoose')
 
-//middleware
+// middleware
 app.use(cors())
 app.use(express.json())
 
 // mongoose
-const URI = 'mongodb+srv://youtubeblocker:youtubeblocker@cluster0.d8vf7.mongodb.net/<dbname>?retryWrites=true&w=majority'
+let USERNAME = process.env.USERNAME
+let PASSWORD = process.env.PASSWORD
+const URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.d8vf7.mongodb.net/<dbname>?retryWrites=true&w=majority`
 const db = mongoose.connection
+mongoose.connect(URI,{useNewUrlParser: true})
+db.on('error',(error) => console.error(error))
+db.once('open',() => console.log('Connected to Database'))
 
-mongoose.connect(URI, { useNewUrlParser: true })
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
-
+// route
 const channels = require('./routes/channels')
-app.use('/api', channels)
+app.use('/api',channels)
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server Started'))
+app.listen(PORT,() => console.log('Server Started'))
